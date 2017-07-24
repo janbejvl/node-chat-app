532f832a711f685965019e9718a11d41711c14cc
bejf <hbejvl@gmail.com>
2017-07-24 21:43:41 +0200

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+const expect = require('expect')
+const { generateMessage } = require('./message')
+
+describe('generateMessage', () => {
+  it('Should generate correct message object', () => {
+    const from = 'Honza'
+    const text = 'This is test from Honza.'
+    const message = generateMessage(from, text)
+
+    expect(message.createdAt).toBeA('number')
+    expect(message).toInclude({ from, text })
+  })
+})
+
